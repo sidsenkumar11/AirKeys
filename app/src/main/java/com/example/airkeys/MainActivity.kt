42d8d00b6928c90e4f4edddffa65c94fe9ec65c3
Chinmay Garg <chinmaygarg@me.com>
2019-05-02 17:36:04 -0700

    gitignore & mainActivity test

--- app/src/main/java/com/example/airkeys/MainActivity.kt
+++ app/src/main/java/com/example/airkeys/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.airkeys
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import org.opencv.android.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if(OpenCVLoader.initDebug()){
+            Toast.makeText(this, "OpenCV successfully loaded", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "OpenCV cannot be loaded", Toast.LENGTH_SHORT).show();
+        }
     }
 }
