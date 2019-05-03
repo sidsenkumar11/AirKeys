95eaf67f25dea2fe2a23112b4d6a3c8061acb8b6
Siddarth Senthilkumar <sidsenkumar11@gmail.com>
2019-05-02 17:40:35 -0700

    update

--- app/src/main/java/com/example/airkeys/MainActivity.kt
+++ app/src/main/java/com/example/airkeys/MainActivity.kt
@@ -2,11 +2,18 @@ package com.example.airkeys
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (OpenCVLoader.initDebug()) {
+            Toast.makeText(this, "openCv successfully loaded", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "openCv cannot be loaded", Toast.LENGTH_SHORT).show()
+        }
     }
 }
